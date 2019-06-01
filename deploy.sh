7bb84c166ca501710b238c9da6e28d9ae60464f5
kim yang <942326994@qq.com>
2019-06-01 11:39:21 +0800

    change travisCI second

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+git config --global credential.helper store
+git config --global user.email "kim_yang95@163.com"
+git config --global user.name "KimYangOfCat"
+git config --global push.default simple
+
+rm -rf deployment
+git clone -b master https://KimYangOfCat:$GITHUB_TOKEN@github.com/KimYangOfCat/KimYangOfCat.github.io deployment
+rsync -av --delete --exclude ".git" public/ deployment
+cd deployment
+git add -A
+# we need the || true, as sometimes you do not have any content changes
+# and git woundn't commit and you don't want to break the CI because of that
+git commit -m "rebuilding site on `date`, commit ${TRAVIS_COMMIT} and job ${TRAVIS_JOB_NUMBER}" || true
+git push
+
+cd ..
+rm -rf deployment
