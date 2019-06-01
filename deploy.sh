78d195d64333dd4386c19fc4050897e96845514f
kim yang <942326994@qq.com>
2019-06-01 16:02:33 +0800

    change travisCI

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git config --global push.default simple
 
 rm -rf deployment
 git clone -b master https://KimYangOfCat:$GITHUB_TOKEN@github.com/KimYangOfCat/KimYangOfCat.github.io deployment
-rsync -av --exclude ".git" public/ deployment
+rsync -av --delete --exclude ".git" --exclude "CNAME" public/ deployment
 cd deployment
 git add -A
 # we need the || true, as sometimes you do not have any content changes
