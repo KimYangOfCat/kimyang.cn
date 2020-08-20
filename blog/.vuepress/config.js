7d05d9fff1b3bf64dc299bfd8f3a16071e4b757d
Kim Yang <kim_yang95@163.com>
2020-08-21 00:55:40 +0800

    :pencil: add icons

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -5,28 +5,35 @@ module.exports = {
     theme: 'reco',
     port: 2333,
     head: [
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
+        ['link', {rel: 'icon', href: '/favicon.ico', type: "image/x-icon"}],
+        ['link', {rel: ' short icon', href: '/favicon.ico', type: "image/x-icon"}],
+        ['link', {rel: 'apple-touch-icon', href: `/favicon.ico`, type: "image/x-icon"}],
     ],
     themeConfig: {
-        type:'blog',
+        type: 'blog',
         authorAvatar: '/avatar.jpg',
         mode: 'auto', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
         modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
         noFoundPageByTencent: false,//关闭404
         codeTheme: 'tomorrow',
         nav: [
-            { text: 'Home', link: '/',icon:'reco-home' },
-            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-            { text: 'Friends', link: '/friends', icon: 'reco-message' },
-            { text: 'GitHub', link: 'https://github.com/KimYangOfCat', icon: 'reco-github' },
+            {text: 'Home', link: '/', icon: 'reco-home'},
+            {text: 'TimeLine', link: '/timeline/', icon: 'reco-date'},
+            {text: 'Friends', link: '/friends', icon: 'reco-message'},
+            {text: 'GitHub', link: 'https://github.com/KimYangOfCat', icon: 'reco-github'},
             {
                 text: 'other',
                 ariaLabel: 'Other Menu',
-                items:[
-                    { text: '知乎', link: 'https://www.zhihu.com/people/kim_yang', icon: 'reco-zhihu' },
-                    { text: 'CSDN', link: 'https://me.csdn.net/weixin_43316691', icon: 'reco-csdn' },
-                    { text: '微博', link: 'https://weibo.com/6373489690/profile?topnav=1&wvr=6&is_all=1', icon: 'reco-weibo' },
-                    { text: 'Email', link: 'mailto:Kim.Yang.YJ@outlook.com', icon: 'reco-mail' },
+                items: [
+                    {text: '知乎', link: 'https://www.zhihu.com/people/kim_yang', icon: 'reco-zhihu'},
+                    {text: 'CSDN', link: 'https://me.csdn.net/weixin_43316691', icon: 'reco-csdn'},
+                    {
+                        text: '微博',
+                        link: 'https://weibo.com/6373489690/profile?topnav=1&wvr=6&is_all=1',
+                        icon: 'reco-weibo'
+                    },
+                    {text: 'Email', link: 'mailto:Kim.Yang.YJ@outlook.com', icon: 'reco-mail'},
                 ]
             }
             // { text: 'sidebar', link: '/views/sidebar/' },
