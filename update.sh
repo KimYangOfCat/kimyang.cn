aa119ed3bddec7ba5c401cc9dc87687a568161b7
Kim Yang <kim_yang95@163.com>
2020-08-22 03:11:58 +0800

    :wrench: finish  shell

--- update.sh
+++ update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#beta
 git checkout beta
 git reset --hard HEAD^
 git merge dev
 rm -rf ./blog/draft/
 git add .
 gitmoji -c
+#master
 git checkout master
-#git merge beta
-#git push
+git merge beta
+git push
+#dev
+git checkout dev
+git reset --hard HEAD
