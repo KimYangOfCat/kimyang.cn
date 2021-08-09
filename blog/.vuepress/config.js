4a438e0001f9e12349ac44f0cdbbc813459210b5
KimYang <Kim.Yang.GH@outlook.com>
2021-08-09 21:51:35 +0800

    change nav

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -39,12 +39,16 @@ module.exports = {
         editLinkText: '发现错误？欢迎斧正',
         nav: [
             { text: '首页', link: '/', icon: 'reco-home' },
-            {
-                text: '其他', items: [
-                    { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
-                    { text: '标签', link: '/tag/', icon: 'reco-tag' },
-                ], icon: 'reco-category'
-            },
+            // {
+            //    // 合并
+            //     text: '其他', items: [
+            //         { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
+            //         { text: '标签', link: '/tag/', icon: 'reco-tag' },
+            //         { text: 'RSS', link: 'https://kimyang.cn/rss.xml', icon: 'reco-rss' },
+            //     ], icon: 'reco-category'
+            // },
+            { text: '标签', link: '/tag/', icon: 'reco-tag' },
+            { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
             { text: 'RSS', link: 'https://kimyang.cn/rss.xml', icon: 'reco-rss' },
         ],
         sidebar: {
