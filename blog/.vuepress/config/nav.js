d7ee50a03257c422f42093ce66fea914d46c8cf4
kim <kim@kimdeMacBook-Pro.local>
2021-08-23 15:16:16 +0800

    :sparkles: add projects

--- blog/.vuepress/config/nav.js
+++ blog/.vuepress/config/nav.js
@@ -3,6 +3,15 @@ module.exports=[
     { text: '算法题解', link: '/algorithm/', icon: 'reco-message' },
     {
         // 合并
+        text: '项目合集', items: [
+            { text: '贪吃蛇游戏', link: 'https://snake.kimyang.cn/'},
+            { text: '打地鼠游戏', link: 'https://mole.kimyang.cn/'},
+            { text: '408 笔记', link: 'https://408.kimyang.cn/'},
+            // { text: '数学笔记', link: 'https://math.kimyang.cn/'},
+        ], icon: 'reco-category'
+    },
+    {
+        // 合并
         text: '其他', items: [
             { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
             { text: '标签', link: '/tag/', icon: 'reco-tag' },
