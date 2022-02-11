2bc268108c3caf05efb70ffcaa1e1143c6c50ccd
KimYangOfCat <kim.yang.gh@outlook.com>
2022-02-11 21:37:53 +0800

    add solution

---
+++ blog/algorithm/src/js/94.二叉树的中序遍历.js
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.66%)
+ * Likes:    1258
+ * Dislikes: 0
+ * Total Accepted:    678.8K
+ * Total Submissions: 897.2K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ function inorderTraversal(root) {
+    const stack = []; //存储节点的栈
+    const res = []; //存储中序的数组
+  
+    //迭代
+    while (root || stack.length) {
+      if (root) {
+        //不断找极左点，将中间点压入栈中
+        stack.push(root);
+        root = root.left;
+      } else {
+        root = stack.pop();
+        res.push(root.val);
+        root = root.right;
+      }
+    }
+  
+    return res;
+  }
+// @lc code=start
+function inorderTraversal(root){
+
+}
+  // @lc code=end
+
