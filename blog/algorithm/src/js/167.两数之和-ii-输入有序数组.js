5baf4fbfbbc326dcd212341e2a760b106ba6fcee
KimYangOfCat <kim.yang.gh@outlook.com>
2022-02-12 14:58:34 +0800

    feat: add  .prettierc and  add solutions of 167 and 283

---
+++ blog/algorithm/src/js/167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ *
+ * https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
+ *
+ * algorithms
+ * Medium (58.71%)
+ * Likes:    679
+ * Dislikes: 0
+ * Total Accepted:    362.4K
+ * Total Submissions: 617.2K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target
+ * 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <=
+ * numbers.length 。
+ *
+ * 以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+ *
+ * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+ *
+ * 你所设计的解决方案必须只使用常量级的额外空间。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：numbers = [2,7,11,15], target = 9
+ * 输出：[1,2]
+ * 解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：numbers = [2,3,4], target = 6
+ * 输出：[1,3]
+ * 解释：2 与 4 之和等于目标数 6 。因此 index1 = 1, index2 = 3 。返回 [1, 3] 。
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：numbers = [-1,0], target = -1
+ * 输出：[1,2]
+ * 解释：-1 与 0 之和等于目标数 -1 。因此 index1 = 1, index2 = 2 。返回 [1, 2] 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 2 <= numbers.length <= 3 * 10^4
+ * -1000 <= numbers[i] <= 1000
+ * numbers 按 非递减顺序 排列
+ * -1000 <= target <= 1000
+ * 仅存在一个有效答案
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+	let l = 0;
+	let r = numbers.length - 1;
+	while (r > 0) {
+		let tem = target - numbers[r];
+		while (l < r) {
+			if (numbers[l] == tem) {
+				break;
+			}
+			l++;
+		}
+		if (l < r) {
+			break;
+		}
+		l = 0;
+		r--;
+	}
+	return [l + 1, r + 1];
+};
+// @lc code=end
---
+++ blog/algorithm/src/js/283.移动零.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ *
+ * https://leetcode-cn.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (64.02%)
+ * Likes:    1427
+ * Dislikes: 0
+ * Total Accepted:    617.8K
+ * Total Submissions: 964.9K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [0]
+ * 输出: [0]
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能尽量减少完成的操作次数吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+	let l = 0;
+	let r = l;
+	while (l < nums.length) {
+		if (nums[l] != 0) {
+			l++;
+		} else {
+			r = l + 1;
+			while (r < nums.length) {
+				if (nums[r] == 0) {
+					r++;
+				} else {
+					let tem = nums[l];
+					nums[l] = nums[r];
+					nums[r] = tem;
+					break;
+				}
+			}
+			l++;
+		}
+	}
+};
+// @lc code=end
